98c6f7f72aaa4eda4e9eced70ca6f7bbfd8597e7
fatihakkoyunadu <fatih.akkoyun@adu.edu.tr>
2020-04-08 11:30:25 +0300

    1D 2D Arrays week9 app1

---
+++ W9A1/W9A1/main.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int main() {
+	
+
+	int myArr[3][4] = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+	int val1 = 0;
+
+	//printf("Size of Array: %i\n\r", sizeof(myArr));
+	//printf("Size of Array[0]: %i\n\r", sizeof(myArr[0]));
+	//printf("Size of Array[0][0]: %i\n\r", sizeof(myArr[0][0]));
+
+	printf("Size of Array[0]: %i\n\r", sizeof(myArr) / sizeof(myArr[0]));	//for Rows
+	printf("Size of Array[0][0]: %i\n\r", sizeof(myArr[0]) / sizeof(myArr[0][0]));	// for Cols
+
+	for (int i = 0; i < sizeof(myArr) / sizeof(myArr[0]); i++){
+		for (int j = 0; j < sizeof(myArr[0]) / sizeof(myArr[0][0]); j++){
+			printf("myArr[%i][%i]=%i\t", i, j, myArr[i][j]);
+		}
+		printf("\n\r");
+	}
+	//	myArr[i][]
+
+	//////init Array
+	////for (int i = 0; i < sizeof(myArr)/sizeof(myArr[0]); i++){
+	////	printf("%i degeri giriniz:",i);
+	////	scanf("%d", &val1);
+	////	myArr[i] = val1;
+	////}
+
+	////for (int i = 0; i < sizeof(myArr) / sizeof(myArr[0]); i++){
+	////	myArr[i] = myArr[i] * 2;  //*=
+	////}
+
+	////for (int i = 0; i < sizeof(myArr) / sizeof(myArr[0]); i++){
+	////	printf("myArr[%d] degeri: %i\n\r", i, myArr[i]);
+	////}
+
+
+
+	getchar();
+	getchar();
+	return (0);
+}
